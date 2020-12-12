1914717efec360fe0444fe4f6a29b7171b7d4909
jbelafa <joel.belafa@dataiku.com>
2020-12-12 22:47:10 +0100

    makes export compatible with FSProviders

--- custom-recipes/export-to-sftp/recipe.py
+++ custom-recipes/export-to-sftp/recipe.py
@@ -5,28 +5,44 @@ from dataikuapi.dssclient import DSSClient
 import pandas as pd
 import json
 from sftp_client import SftpConnector
+import shutil
 
 print ("Custom Recipe being")
 
 # get recipe config
 sftp_service = get_recipe_config()['sftpService']
-sftp_root_path = get_recipe_config()['sftp_root_path']
+sftp_root_path = get_recipe_config()['sftp_root_path']# we keep the name for migration purpose
+bufsize = get_recipe_config()['bufsize']
 
-# load connector
+# Load connector from  python-lib
 sftp_connector = SftpConnector(sftp_service)
 
 # get the full location of local folder
 source_folder = dataiku.Folder(get_input_names_for_role('folder_with_data')[0])
-source_folder_path = source_folder.get_path()
+
+# get the full location of dest_folder
+dest_folder = dataiku.Folder(get_output_names_for_role('sftp_folder')[0])
+
+if dest_folder.get_info().get("type") != "fsprovider_sftp-python_sftp-python_sftp-fs-provider":
+    raise TypeError(" This recipe expects a sftp-python folder as an output")
 
 
-# Start sync 
-list_of_files_updated = sftp_connector.upload_dir(source_folder_path,sftp_root_path)
+# Helpers
+def sync_path(origin):
+    details = source_folder.get_path_details(origin)
+    print("start downloading : {}".format(details.get("fullPath")))
 
+    if (details.get("directory")):
+        for child in details.get("children"):
+            sync_path(child.get("fullPath"))
+    else:
+        clone_path = details.get("fullPath").replace(sftp_root_path,"",1) or sftp_root_path
+        with source_folder.get_download_stream(details.get("fullPath")) as source_file:
+            with dest_folder.get_writer(clone_path) as dest_file:
+                shutil.copyfileobj(source_file, dest_file,bufsize)
+    
+    print("Finished downloading: {}".format(details.get("fullPath")))
+    return
 
-#Save the 
-out_df = pd.DataFrame()
-out_df['Files Updated'] = list_of_files_updated
-
-output_dataset = dataiku.Dataset(get_output_names_for_role('export_details')[0])
-output_dataset.write_with_schema(out_df)
+# MAIN PROCESSING
+sync_path(sftp_root_path)
