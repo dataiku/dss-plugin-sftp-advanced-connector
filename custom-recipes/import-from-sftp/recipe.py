bef42c17534c31085db9418929df80af681e37d1
jbelafa <joel.belafa@dataiku.com>
2020-12-12 19:46:58 +0100

    add input folder as parameter

--- custom-recipes/import-from-sftp/recipe.py
+++ custom-recipes/import-from-sftp/recipe.py
@@ -15,10 +15,15 @@ sftp_root_path = get_recipe_config()['sftp_root_path']
 sftp_connector = SftpConnector(sftp_service)
 
 # get the full location of local folder
+source_folder = dataiku.Folder(get_input_names_for_role('sftp_source_folder')[0])
+source_folder_path = source_folder.get_path()
+
+
+# get the full location of local folder
 folder = dataiku.Folder(get_output_names_for_role('folder_with_data')[0])
 folder_path = folder.get_path()
 
 # Start sync 
-sftp_connector.download_dir(folder_path,sftp_root_path)
+sftp_connector.download_dir(folder_path,source_folder_path)
 
 print ("Recipe complete")
